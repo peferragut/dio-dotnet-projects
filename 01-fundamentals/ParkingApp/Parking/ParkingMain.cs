00c324eb99aaf843b26dcc15644ee5434a10e2bd
Pedro Ferragut <pefero2000@gmail.com>
2024-07-30 20:39:44 -0300

    Init project

---
+++ 01-fundamentals/ParkingApp/Parking/ParkingMain.cs
@@ -0,0 +1,45 @@
+﻿namespace Parking
+{
+    public class ParkingMain
+    {
+
+        private string? _input;
+        private static readonly string[] OPTIONS = { "1", "2", "3", "4" };
+        private ParkingManager? parkingManager;
+
+        static void Main(string[] args)
+        {
+            ParkingMain parkingMain = new ParkingMain();
+            parkingMain.Start();            
+        }
+
+        // private
+        private void Start() 
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Kundera Parking app!");
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine("1 - Add a vehicle");
+            Console.WriteLine("2 - Remove a vehicle");
+            Console.WriteLine("3 - List all parked vehicles");
+            Console.WriteLine("4 - Finish");
+
+            parkingManager = new ParkingManager();
+
+            while (true)
+            {
+                _input = Console.ReadLine();
+                if (_input != null)
+                {
+                    if (!OPTIONS.Contains(_input))
+                    {
+                        Console.WriteLine("Invalid input! Choose between options from 1 to 4.");
+                        continue;
+                    }
+                    
+                    parkingManager.ProcessOption(_input);
+                }
+            }
+        }
+    }
+}
---
+++ 01-fundamentals/ParkingApp/Parking/ParkingManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Parking
+{
+    internal class ParkingManager
+    {
+        public ParkingManager() { }
+        public void ProcessOption(string option)
+        {
+            switch (option)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
